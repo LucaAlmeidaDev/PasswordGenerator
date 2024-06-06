2e0f7acce8588bef88e346912caf08f9b1cad7ca
Lucas Almeida <124281689+LucaAlmeidaDev@users.noreply.github.com>
2024-06-06 15:10:42 -0300

    minor changes

--- script.js
+++ script.js
@@ -1,38 +1,86 @@
 let Generate = document.getElementById('generateBtn')
 let CheckBoxNum = document.getElementById('numbers')
 let CheckBoxSpecial = document.getElementById('special')
-let CheckBoxLower = document.getElementById('upperCase')
-let CheckBoxUpper = document.getElementById('lowerCase')
+let CheckBoxLower = document.getElementById('lowerCase')
+let CheckBoxUpper = document.getElementById('upperCase')
 let Password = document.getElementById('passwordText')
+var range = document.getElementById('passwordSize');
+var value = document.getElementById('passwordSizeLabel');
+
+var pass = '';
+special = "!?@#$-_";
+specialChecker = 0;
+number = "0123456789";
+numberChecker = 0;
+upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+upperChecker = 0;
+lower = 'abcdefghijklmnopqrstuvwxyz';
+lowerChecker = 0;
 
 
-function getRandomNum(max){
-    return Math.floor(Math.random() * max);
-}
+range.addEventListener('input', function() {
+    value.textContent = this.value;
+});
 
-function checkBoxSpecial(){
-    if(CheckBoxSpecial.checked === true){
-        console.log('checked')
+function checkBoxLowerHandler(){
+    if(CheckBoxLower.checked === true){
+        lowerChecker = 1;
+        console.log(lowerChecker)
     }else{
-        console.log('false')
+        lowerChecker = 0;
+        console.log(lowerChecker)
     }
 }
-
+function checkBoxUpperHandler(){
+    if(CheckBoxUpper.checked === true){
+        upperChecker = 1;
+        console.log(upperChecker)
+    }else{
+        upperChecker = 0;
+        console.log(upperChecker)
+    }
+}
 function checkBoxNumHandler(){
     if(CheckBoxNum.checked === true){
-        console.log(getRandomNum(10));
+        numberChecker = 1;
+        console.log(numberChecker)
     }else{
-        console.log('none')
+        numberChecker = 0;
+        console.log(numberChecker)
     }
 }
-
+function checkBoxSpecialHandler(){
+    if(CheckBoxSpecial.checked === true){
+        specialChecker = 1;
+        console.log(specialChecker)
+    }else{
+        specialChecker = 0;
+        console.log(specialChecker)
+    }
+}
 function generateRandomPassword(){
-    Password.innerHTML = 'ziririr';
+    checkBoxNumHandler()
+    checkBoxSpecialHandler()
+    checkBoxUpperHandler()
+    checkBoxLowerHandler()
+    
+    if(numberChecker + specialChecker + upperChecker + lowerChecker === 0 ){
+        alert('Select a filter option');
+    
+    }else if(numberChecker === 1){
+
+    }else if(specialChecker === 1){
+
+    }else if(upperChecker === 1){
+
+    }else if(lowerChecker === 1){
+
+    }
+
+
 }
 
 
 Generate.onclick = function(){
-    checkBoxSpecial()
     generateRandomPassword()     
-    checkBoxNumHandler()
 }

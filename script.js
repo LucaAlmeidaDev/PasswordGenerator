3d3e75c83dab80c1ecf5fea0a8f423adcb83e4ad
Lucas Almeida <124281689+LucaAlmeidaDev@users.noreply.github.com>
2024-06-12 12:40:31 -0300

    Implementado a lógica do password Generator

--- script.js
+++ script.js
@@ -4,83 +4,87 @@ let CheckBoxSpecial = document.getElementById('special')
 let CheckBoxLower = document.getElementById('lowerCase')
 let CheckBoxUpper = document.getElementById('upperCase')
 let Password = document.getElementById('passwordText')
+let CopyButton = document.getElementById('Copy')
 var range = document.getElementById('passwordSize');
 var value = document.getElementById('passwordSizeLabel');
 
-var pass = '';
-special = "!?@#$-_";
-specialChecker = 0;
-number = "0123456789";
-numberChecker = 0;
-upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-upperChecker = 0;
-lower = 'abcdefghijklmnopqrstuvwxyz';
-lowerChecker = 0;
 
+lowerChecker = false;
+upperChecker = false;
+numChecker = false;
+specialChecker = false;
+
+var rangesize;
 
 range.addEventListener('input', function() {
     value.textContent = this.value;
+    rangesize = this.value
 });
 
-function checkBoxLowerHandler(){
+function checkLowerhandler(){
     if(CheckBoxLower.checked === true){
-        lowerChecker = 1;
-        console.log(lowerChecker)
+        lowerChecker = true;
     }else{
-        lowerChecker = 0;
-        console.log(lowerChecker)
+        lowerChecker = false;
     }
 }
-function checkBoxUpperHandler(){
+function checkUpperhandler(){
     if(CheckBoxUpper.checked === true){
-        upperChecker = 1;
-        console.log(upperChecker)
+        upperChecker = true;
     }else{
-        upperChecker = 0;
-        console.log(upperChecker)
+        upperChecker = false;
     }
 }
-function checkBoxNumHandler(){
+function checkNumhandler(){
     if(CheckBoxNum.checked === true){
-        numberChecker = 1;
-        console.log(numberChecker)
+        numChecker = true;
     }else{
-        numberChecker = 0;
-        console.log(numberChecker)
+        numChecker = false;
     }
 }
-function checkBoxSpecialHandler(){
+function checkSpecialhandler(){
     if(CheckBoxSpecial.checked === true){
-        specialChecker = 1;
-        console.log(specialChecker)
+        specChecker = true;
     }else{
-        specialChecker = 0;
-        console.log(specialChecker)
+        specChecker = false;
     }
 }
-function generateRandomPassword(){
-    checkBoxNumHandler()
-    checkBoxSpecialHandler()
-    checkBoxUpperHandler()
-    checkBoxLowerHandler()
-    
-    if(numberChecker + specialChecker + upperChecker + lowerChecker === 0 ){
-        alert('Select a filter option');
-    
-    }else if(numberChecker === 1){
 
-    }else if(specialChecker === 1){
+function generatePassword(){
+    checkLowerhandler()
+    checkUpperhandler()
+    checkNumhandler()
+    checkSpecialhandler()
 
-    }else if(upperChecker === 1){
+    const lowerChar = 'abcdefghijklmnopqrstuvwxyz';
+    const upperChar = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const numChar = '0123456789'; 
+    const specialChar = '!-_?()/&%';
 
-    }else if(lowerChecker === 1){
+    let selectedChars = '';
+    let pass = ''
 
+    selectedChars += lowerChecker ? lowerChar : '';
+    selectedChars += upperChecker ? upperChar : '';
+    selectedChars += numChecker ? numChar : '';
+    selectedChars += specChecker ? specialChar : '';
+
+    for(let i = 0; i < rangesize; i++){
+        const randomIndex = Math.floor(Math.random() * selectedChars.length);
+        pass += selectedChars[randomIndex];
     }
+    Password.innerHTML = pass;
 
-
+    CopyButton.onclick = function(){
+        navigator.clipboard.writeText(pass);
+        alert("Copied the password: " + pass);
+    }
+    
 }
-
 
 Generate.onclick = function(){
-    generateRandomPassword()     
+    generatePassword();
 }
+
+
+
